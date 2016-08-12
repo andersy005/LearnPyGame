14caf762fd15e7696ebab900f94ca32c0c43eac2
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-08-12 07:38:13 -0500

    getBoxAtPixel( )

--- Memory-Puzzle/memoryPuzzle.py
+++ Memory-Puzzle/memoryPuzzle.py
@@ -186,3 +186,15 @@ def leftTopCoordsOfBox(boxx, boxy):
     left = boxx * (BOXSIZE + GAPSIZE) + XMARGIN
     top = boxy * (BOXSIZE + GAPSIZE) + YMARGIN
     return left, top
+
+
+def getBoxAtPixel(x, y):
+    for boxx in range (BOARDWIDTH):
+        for boxy in range(BOARDHEIGHT):
+            left, top = leftTopCoordsOfBox(boxx, boxy)
+            boxRect = pygame.Rect(left, top, BOXSIZE, BOXSIZE)
+            if boxRect.collidepoint(x, y):
+                return boxx, boxy
+
+    return None, None
+
