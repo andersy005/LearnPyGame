f78d8f8f81e3042dded6449f6ea92a72a05525d6
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-08-12 12:34:55 -0500

    drawHighlightBox( )

--- Memory-Puzzle/memoryPuzzle.py
+++ Memory-Puzzle/memoryPuzzle.py
@@ -1,4 +1,3 @@
-
 # Memory Puzzle clone
 # By Anderson Banihirwe  
 
@@ -7,7 +6,6 @@ import random
 import pygame
 import sys
 from pygame.locals import *
-
 
 FPS = 30  # frames per second, the general speed of the program
 WINDOWWIDTH = 640  # size of window's width in pixels
@@ -18,7 +16,7 @@ GAPSIZE = 10  # size of gap between boxes in pixels
 BOARDWIDTH = 10  # number of columns of icons
 BOARDHEIGHT = 7  # number of rows of icons
 
-assert(BOARDHEIGHT * BOARDWIDTH) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
+assert (BOARDHEIGHT * BOARDWIDTH) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
 
 XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
@@ -57,8 +55,8 @@ def main():
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode(WINDOWWIDTH, WINDOWHEIGHT)
 
-    mousex = 0   # used to store x coordinate of  mouse event
-    mousey = 0   # used to store y coordinate of mouse event
+    mousex = 0  # used to store x coordinate of  mouse event
+    mousey = 0  # used to store y coordinate of mouse event
     pygame.display.set_caption('Memory Game')
 
     mainBoard = getRandomizedBoard()
@@ -94,8 +92,8 @@ def main():
             if not revealedBoxes[boxx][boxy]:
                 drawHighlightBox(boxx, boxy)
             if not revealedBoxes[boxx][boxy] and mouseClicked:
-                revealedBoxesAnimation(mainBoard, [(boxx, boxy)])
-                revealedBoxes[boxx][boxy] = True # set the box as revealed
+                revealBoxesAnimation(mainBoard, [(boxx, boxy)])
+                revealedBoxes[boxx][boxy] = True  # set the box as revealed
 
                 if firstSelection == None:  # the current box was the first box clicked
                     firstSelection = (boxx, boxy)
@@ -130,7 +128,7 @@ def main():
 
                         # Replay the start game animation.
                         startGameAnimation(mainBoard)
-                    firstSelection = None # reset firstSelection variable
+                    firstSelection = None  # reset firstSelection variable
 
         # Redraw the screen and wait a clock tick
         pygame.display.update()
@@ -143,6 +141,7 @@ def generateRevealedBoxesData(val):
         revealedBoxes.append([val] * BOARDHEIGHT)
 
     return revealedBoxes
+
 
 def getRandomizedBoard():
     # Get a list of every possible shape in every possible color.
@@ -160,7 +159,7 @@ def getRandomizedBoard():
     # Create the board data structure, with randomly placed icons.
 
     board = []
-    for x in range(BOARDWIDTH ):
+    for x in range(BOARDWIDTH):
         column = []
         for y in range(BOARDHEIGHT):
             column.append(icons[0])
@@ -189,7 +188,7 @@ def leftTopCoordsOfBox(boxx, boxy):
 
 
 def getBoxAtPixel(x, y):
-    for boxx in range (BOARDWIDTH):
+    for boxx in range(BOARDWIDTH):
         for boxy in range(BOARDHEIGHT):
             left, top = leftTopCoordsOfBox(boxx, boxy)
             boxRect = pygame.Rect(left, top, BOXSIZE, BOXSIZE)
@@ -267,19 +266,19 @@ def drawBoard(board, revealed):
     # Draws all of the boxes in their covered or revealed state.
 
     for boxx in range(BOARDWIDTH):
-        if __name__ == '__main__':
-            if __name__ == '__main__':
-                for boxy in range(BOARDHEIGHT):
-                    left, top = leftTopCoordsOfBox(boxx, boxy)
-                    if not revealed[boxx][boxy]:
-                        # Draw a covered box
-                        pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top, BOXSIZE, BOXSIZE))
+        for boxy in range(BOARDHEIGHT):
+            left, top = leftTopCoordsOfBox(boxx, boxy)
+            if not revealed[boxx][boxy]:
+                # Draw a covered box
+                pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top, BOXSIZE, BOXSIZE))
 
-                    else:
-                        # Draw the (revealed) icon
-                        shape, color = getShapeAndColor(board, boxx, boxy)
-                        drawIcon(shape, color, boxx, boxy)
+            else:
+                # Draw the (revealed) icon
+                shape, color = getShapeAndColor(board, boxx, boxy)
+                drawIcon(shape, color, boxx, boxy)
 
 
-
-
+def drawHighlightBox(boxx, boxy):
+    left, top = leftTopCoordsOfBox(boxx, boxy)
+    pygame.draw.rect(DISPLAYSURF, HIGHLIGHTCOLOR, (left - 5, top - 5,
+                                                   BOXSIZE + 10, BOXSIZE + 10), 4)
